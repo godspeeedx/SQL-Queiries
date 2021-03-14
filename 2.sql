a8a8518c594024a52a9da7dc3c065471f6fbe8f4
godspeeedx <smirnov3g@yandex.ru>
2021-03-15 01:18:40 +0300

    first iteration, first lab

---
+++ 2.sql
@@ -0,0 +1,18 @@
+﻿
+--2. Для каждого преподавателя вывести его имя, фамилию, наименование соответствующего подразделения и наименования всех преподаваемых предметов.
+--   Если для преподавателя нет информации о преподаваемых предметах, вывести «-».
+--
+
+select pers.person_family surname,
+  pers.person_name _name,
+  struct.univer_str_name kafedra,
+  COALESCE(obj.obj_name, '-') subject
+from tperson pers
+  INNER join tcl_status status on
+	(
+	  pers.status_id = status.status_id AND
+	  status.status_name = 'Prepod'
+	)
+  INNER JOIN tuniver_str struct ON (pers.univer_str_id = struct.univer_str_id)
+  LEFT join tperson_obj pred on (pers.person_id = pred.person_id)
+  LEFT join tcl_obj obj on (pred.obj_id = obj.obj_id)
---
+++ 7.sql
@@ -0,0 +1,30 @@
+﻿--7. Сформировать отчет, описывающий изучаемые на кафедре предметы: 
+--    название подразделения – название предмета - признак изучения «+»/«-».
+--
+
+-- select distinct
+--   struct.univer_str_name kafedra,
+--   --rasp.obj_id,
+--   --rasp2.univer_str_id,
+--   rasp2.obj_id
+-- from tuniver_obj rasp
+--   CROSS join tuniver_obj rasp2
+--   INNER JOIN tuniver_str struct on (rasp.univer_str_id = struct.univer_str_id)
+
+-- select * from tuniver_obj
+-- select * from tuniver_str
+
+select DISTINCT
+  struct.univer_str_name kafedra,
+  obj.obj_name predmet,
+  coalesce(obj2.obj_name, '-') predmet
+From tuniver_obj rasp
+  CROSS join tuniver_obj rasp2
+  INNER JOIN tuniver_str struct on (rasp.univer_str_id = struct.univer_str_id)
+  INNER JOIN tcl_obj obj on (rasp2.obj_id = obj.obj_id)
+  LEFT join tuniver_obj on_kaf on
+  (
+    on_kaf.univer_str_id = rasp.univer_str_id AND
+    on_kaf.obj_id = rasp2.obj_id
+  )
+  left JOIN tcl_obj obj2 on (on_kaf.obj_id = obj2.obj_id)
